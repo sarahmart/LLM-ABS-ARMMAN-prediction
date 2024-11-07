7b2faf14888d55e60d949179fe84fa3ec9f79ef7
S. Mart <sarahmartinson@g.harvard.edu>
2024-11-06 22:58:18 -0500

    Updating bash scripts to run models separately

---
+++ anthropic_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for X in {0..9}
+do
+    python3 models/main.py --num_arms 100 --config_path anthropic_config.json --t1 $X --t2 $X
+done
+
+echo "All runs completed."
--- meta_run.sh
+++ meta_run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Loop through values from 0 to 9
 for X in {0..9}
 do
-    # Run the python command with --t1 and --t2 set to X
     python3 models/main.py --num_arms 100 --config_path meta_config.json --t1 $X --t2 $X
 done
 
+echo "All runs completed."
